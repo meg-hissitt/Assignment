0887ad87df180cc3fd0cafe3df6ba1ccac35c712
Meg <c4047676@hallam.shu.ac.uk>
2024-12-11 10:31:08 +0000

    Add files via upload
    new

--- Hospital System2/Hospital System2/Program.cs
+++ Hospital System2/Hospital System2/Program.cs
@@ -2,57 +2,67 @@
 {
     using Newtonsoft.Json;
     using System;
-
+    using System.ComponentModel.Design;
+    using System.Runtime.CompilerServices;
+    using static Hospital_System2.UserManager;
+    
     class Program
     {
-        public class Patient
-        {
-            //can rename classes by rightclicking them to rename them everywhere
-            //create doctor/nurse/admin role and number(or other) them to select within them - made staff class + added role enum
-            //if statements for what they can do 
-            public string Username { get; set; }
-            public string Password { get; set; }
-            public string Name { get; set; }
-            public string PatientID { get; set; }
-            public string DoB { get; set; }
-
-        }
+        private static Staff CurrentUser = null; //TODO change Patient to Staff
 
         string usersFilePath = @"users.json";
         public static void Main(string[] args)
         {
             UserManager uman = new UserManager();
 
-            uman.LoadUsers();
+            //uman.LoadStaffMembers();
 
             bool running = true;
             while (running)
             {
-                //Console.Clear();
+                
+                Console.Clear();
                 Console.WriteLine("1. Login");
-                Console.WriteLine("2. Register");
-                Console.Write("What would you like to do? ");
-                //Console.WriteLine("Reset password"); - replace the register option with this 
+                Console.WriteLine("2. Exit");
+                Console.Write("Select a menu option:  ");
+                //Console.WriteLine("Reset password"); 
 
                 string choice = Console.ReadLine();
 
+
+
                 if (choice == "1")
                 {
-                    uman.Login();
+                    CurrentUser = uman.Login();
+                    if (CurrentUser != null)
+                    {
+                        Console.WriteLine("Login success.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid username or password.");
+                    }
                 }
                 else if (choice == "2")
                 {
-                    uman.Register();
+                    //uman.PatientRegister();
+                    running = false;
+                    CurrentUser = null;
                 }
                 else
                 {
                     Console.WriteLine("Invalid input");
                 }
-            
+
+                while(CurrentUser != null)
+                {
+                    UserActions(CurrentUser);
+                }
             }
         }
 
-        private static void userActions (Staff user)
+        private static void UserActions (Staff user)
+             
         {
             switch (user.StaffRole)
             {
@@ -63,20 +73,99 @@
                     Console.WriteLine("4. Add new staff member");
                     Console.WriteLine("5. Update user");
                     Console.Write("Select a choice:");
-                    string adminChoice = Console.ReadLine();
+                    
+                    int adminChoice = int.Parse(Console.ReadLine());
+
+                    if (adminChoice == 1)
+                    {
+                        if (File.Exists("users.json"))
+                        {
+                            string jsonString = File.ReadAllText("users.json");
+                            List<Patient> patients = JsonConvert.DeserializeObject<List<Patient>>(jsonString).ToList();
+
+                            foreach (Patient patient in patients)
+                            {
+                                Console.WriteLine($"Patient name: {patient.Name}");
+                            }
+                        }
+                    }
+                    else if (adminChoice == 2)
+                    {
+                        Console.WriteLine("Enter a name, date of birth or patient number");
+                        string search = Console.ReadLine();
+                        
+                    }
+                    else if (adminChoice == 3)
+                    {
+                       uman.PatientRegister();
+                    }
+                    else if (adminChoice == 4)
+                    {
+                        Console.Write("");
+                    }
+                    else if (adminChoice == 5)
+                    {
+                        Console.Write("");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid option");
+                    }
+
                     break;
                 case Role.Doctor:
                     Console.WriteLine("1. View All patients");
                     Console.WriteLine("2. Search");
                     Console.WriteLine("3. Add appointment");
-                    string doctorChoice = Console.ReadLine();
-                    break;
+
+                    int doctorChoice = int.Parse(Console.ReadLine());
+
+                    if (doctorChoice == 1)
+                    {
+                        Console.Write("");
+                    }
+                    else if (doctorChoice == 2)
+                    {
+                        Console.Write("");
+                    }
+                    else if (doctorChoice == 3)
+                    {
+                        Console.Write("");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid option");
+                    }
+                        break;
                 case Role.Nurse:
                     Console.WriteLine("1. Add new patient");
                     Console.WriteLine("2. View All patients");
                     Console.WriteLine("3. Search");
                     Console.WriteLine("4. Add appointment");
-                    string nurseChoice = Console.ReadLine();
+
+                    int nurseChoice = int.Parse(Console.ReadLine());
+
+                    if (nurseChoice == 1)
+                    {
+                        Console.Write("");
+                    }
+                    else if (nurseChoice == 2)
+                    {
+                        Console.Write("");
+                    }
+                    else if (nurseChoice == 3)
+                    {
+                        Console.Write("");
+                    }
+                    else if (nurseChoice == 4)
+                    {
+                        Console.Write("");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid option");
+                    }
+
                     break;
                 default:
                     Console.WriteLine("Role not recognized.");
--- Hospital System2/Hospital System2/UserManager.cs
+++ Hospital System2/Hospital System2/UserManager.cs
@@ -11,50 +11,78 @@ namespace Hospital_System2
 {
     class UserManager
     {
-        public string json { get; set; }
-        public void LoadUsers()
-        {
-            if (File.Exists(@"users.json"))
+        private List<Staff> _staffs;
+        public List<Staff> StaffMembers {
+            get {
+                if (_staffs == null)
+                {
+                    _staffs = LoadStaffMembers();
+                }
+                return _staffs;
+            } 
+            set{
+            
+            } }
+        private List<Patient> _patients;
+        public List<Patient> Patients { 
+            get
             {
-                json = File.ReadAllText(@"users.json");
+                if(_patients == null) { _patients = LoadPatients(); }
+                return _patients;
             }
-            else
+            set
             {
-                File.Create(@"users.json").Close();
-                json = File.ReadAllText(@"users.json");
             }
 
         }
-        public bool Login()
+        public List<Staff> LoadStaffMembers()
         {
-            LoadUsers();
-            List<Patient> userList = JsonConvert.DeserializeObject<List<Patient>>(this.json);
+            List<Staff> staffList = new List<Staff>();
+            if (File.Exists(@"users.json"))
+            {
+                string json = File.ReadAllText(@"users.json");
+                staffList = JsonConvert.DeserializeObject<List<Staff>>(json);
+            }
+
+            return staffList;
+        }
+
+        public List<Patient> LoadPatients()
+        {
+            List<Patient> patientList = new List<Patient>();
+            if (File.Exists(@"patient.json"))
+            {
+                string json = File.ReadAllText(@"patient.json");
+                patientList = JsonConvert.DeserializeObject<List<Patient>>(json);
+            }
+
+            return patientList;
+        }
+
+        //TODO: change Patient to Staff
+        public Staff? Login()
+        {
             Console.Write("Username: ");
             string username = Console.ReadLine();
             Console.Write("Password: ");
             string password = Console.ReadLine();
-            foreach (var user in userList)
+
+            foreach (var user in StaffMembers)
             {
-                if (user.Username == username && user.Password == password)
+                if (user.Email == username && user.Password == password)
                 {
-                    Console.WriteLine("Login successfull.");
-                    return true;
-                }
-                else
-                {
-                    Console.WriteLine("Login unsuccesfull");
-                    return false; 
+                    return user;
                 }
             }
-            return false;
+            return null;
 
         }
-        void SaveUsers(List<Patient> users)
+        void SavePatients(List<Patient> users)
         {
             string json = JsonConvert.SerializeObject(users);
             File.WriteAllText(@"users.json", json);
         }
-        public void Register()
+        public void PatientRegister()
         {
             Console.Write("Enter your first and last name: ");
             string name = Console.ReadLine();
@@ -72,17 +100,13 @@ namespace Hospital_System2
             string password = Console.ReadLine();
 
             Console.WriteLine("Select role:");
-            Console.WriteLine("1. Admin");
-            Console.WriteLine("2. Doctor");
-            Console.WriteLine("3. Nurse");
-            Console.WriteLine("4. Patient");
+            Console.WriteLine("1. Patient");
             Console.Write("Choice: ");
             int roleChoice = int.Parse(Console.ReadLine());
 
-            List<Patient> userList = JsonConvert.DeserializeObject<List<Patient>>(this.json);
-            Patient newUser = new Patient {Name = name, DoB = DoB, PatientID = patientID, Username = username, Password = password };
-            userList.Add(newUser);
-            SaveUsers(userList);
+            Patient newUser = new Patient { Name = name, DoB = DoB, PatientID = patientID, Username = username, Password = password };
+            Patients.Add(newUser);
+            SavePatients(Patients);
 
         }
         void SaveStaff(List<Staff> users)
@@ -112,10 +136,27 @@ namespace Hospital_System2
             Console.Write("Choice: ");
             int roleChoice = int.Parse(Console.ReadLine());
 
-            List<Staff> staffList = JsonConvert.DeserializeObject<List<Staff>>(this.json);
-            Staff newStaff = new Staff { Name = staffName,StaffNumber = staffNumber, Email = staffEmail, Password = staffPassword };
-            staffList.Add(newStaff);
-            SaveStaff(staffList);
+            Staff newStaff = new Staff { Name = staffName, StaffNumber = staffNumber, Email = staffEmail, Password = staffPassword };
+            StaffMembers.Add(newStaff);
+            SaveStaff(StaffMembers);
+        }
+        public void PatientSearch()
+        {
+            
+            string name = Console.ReadLine();
+            string dob = Console.ReadLine();
+            string patientID = Console.ReadLine();
+            foreach (var patient in Patients)
+            {
+                if (patient.Name == name || patient.DoB == dob || patient.PatientID == patientID)
+                {
+                    Console.WriteLine("");
+                }
+                else
+                {
+                    Console.WriteLine("No results found");
+                }
+            }
         }
     }
 }
